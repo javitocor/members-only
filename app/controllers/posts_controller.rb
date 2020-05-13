fd5fc05e28c282e9755d88cbb12562fc005231c2
javitocor <javitocor@gmail.com>
2020-05-13 12:11:16 +0200

    Fixed rubocop errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,26 +1,25 @@
 class PostsController < ApplicationController
-    before_action :authenticate_user!, only: [:new, :create]
-    def index 
-        @posts = Post.all.order(create_at: :asc)
+  before_action :authenticate_user!, only: %i[new create]
+  def index
+    @posts = Post.all.order(create_at: :asc)
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to root_path, notice: 'Post created'
+    else
+      render 'new', notice: 'Failed to crete post'
     end
+  end
 
-    def new 
-        @post = Post.new
-    end
+  private
 
-    def create
-        @post = current_user.posts.build(post_params)
-        if @post.save
-            redirect_to root_path, notice: 'Post created'
-        else
-            render 'new', notice: 'Failed to crete post'
-        end
-    end
-
-    private
-
-    def post_params 
-        params.require(:post).permit(:title, :content)
-    end
-
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
 end
