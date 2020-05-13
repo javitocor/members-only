e2a738adb839dd47cf2125279caf82d695476d07
javitocor <javitocor@gmail.com>
2020-05-13 11:53:48 +0200

    Changed to the order of displyed posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
     before_action :authenticate_user!, only: [:new, :create]
     def index 
-        @posts = Post.all.order(create_at: :desc)
+        @posts = Post.all.order(create_at: :asc)
     end
 
     def new 
