03a8025c6b6ad935dbc09c31c22b717b7ee66dee
tirthajyoti-ghosh <itirthahere@gmail.com>
2020-05-14 11:53:26 +0530

    fixed rubocop carriage return errors, added log in helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def enforce_log_in
+    redirect_to new_user_session_path unless user_signed_in?
+  end
+
   protected
 
   def configure_permitted_parameters
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, only: %i[new create]
+  before_action :enforce_log_in, only: %i[new create]
   def index
     @posts = Post.all.order(create_at: :asc)
   end
